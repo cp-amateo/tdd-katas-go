ec10a8f44936f3adbdfe50b87dce0e5b88588a86
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-06-03 23:59:32 +0200

    TextProcessingKata - ✅ analyse Hello world world

--- textProcessing/Processor.go
+++ textProcessing/Processor.go
@@ -1,8 +1,33 @@
 package textProcessing
 
-import "strings"
+import (
+	"sort"
+	"strings"
+)
 
 func analyse(text string) (topWords []string, countWords int) {
 	words := strings.Fields(text)
-	return words, len(words)
+
+	wordsMap := make(map[string]int)
+
+	for _, word := range words {
+		occurrences, found := wordsMap[word]
+		if found {
+			wordsMap[word] = occurrences + 1
+		} else {
+			wordsMap[word] = 1
+		}
+	}
+
+	keys := make([]string, 0, len(wordsMap))
+
+	for key := range wordsMap {
+		keys = append(keys, key)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return wordsMap[keys[i]] > wordsMap[keys[j]]
+	})
+
+	return keys, len(words)
 }
