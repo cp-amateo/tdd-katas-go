8b89f78fbbe8a43875e8781e19414955c71106ef
Alejandro Mateo <alejandro.mateo@clearpay.com>
2022-06-03 23:15:51 +0200

    TextProcessingKata - ❌ analyse Hello

---
+++ textProcessing/Processor_test.go
@@ -0,0 +1,30 @@
+package textProcessing
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_analyse(t *testing.T) {
+	tests := []struct {
+		name           string
+		arg            string
+		wantTopWords   []string
+		wantCountWords int
+	}{
+		{"analyse Hello", "Hello", []string{"Hello"}, 1},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			gotTopWords, gotCountWords := analyse(test.arg)
+			if !reflect.DeepEqual(test.wantTopWords, gotTopWords) {
+				t.Errorf("analyse() gotTopWords = %v, want %v", gotTopWords, test.wantTopWords)
+			}
+			if test.wantCountWords != gotCountWords {
+				t.Errorf("analyse() gotCountWords = %v, want %v", gotCountWords, test.wantCountWords)
+			}
+		})
+	}
+
+}
